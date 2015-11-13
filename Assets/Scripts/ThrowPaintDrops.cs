5afbb8e7d3b1b8e62a640fe26e9d9b32cf035c0a
Vintache Jean <jean.vintache@etu.utc.fr>
2015-11-13 17:43:13 +0100

    Lancer de spheres noires

---
+++ Assets/Scripts/ThrowPaintDrops.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowPaintDrops : MonoBehaviour {
+	public Transform paintDropPrefab;
+
+	private const int RAYCASTLENGTH = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = GetComponentInChildren<Camera>().ScreenPointToRay(Input.mousePosition);
+				
+		Debug.DrawRay(ray.origin, ray.direction * RAYCASTLENGTH, Color.green);
+		
+		if (Input.GetMouseButton(0))
+		{
+			Transform drop = Instantiate(paintDropPrefab, ray.origin, Quaternion.identity) as Transform;
+			drop.gameObject.GetComponent<Rigidbody>().AddForce(ray.direction * 300);
+		}
+
+	}
+}
