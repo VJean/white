ca1f6d1eaf9ee3e06f11ab72a1fa4074230a5b95
atchandj <atchandjou@gmail.com>
2015-11-13 15:28:43 +0100

    Added raycasting for painting

---
+++ Assets/Scripts/RayCasting.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayCasting : MonoBehaviour {
+	
+	public const int RAYCASTLENGTH = 100;	// Longueur du rayon issu de la caméra
+	public GameObject paint;
+	void Start () 
+	{
+	}
+	
+	void Update () 
+	{
+		// Le raycast attache un objet cliqué
+		RaycastHit hitInfo;
+		Ray ray = GetComponentInChildren<Camera>().ScreenPointToRay(Input.mousePosition);
+		Debug.DrawRay (ray.origin, ray.direction * RAYCASTLENGTH, Color.blue);
+		bool rayCasted = Physics.Raycast (ray, out hitInfo, RAYCASTLENGTH);
+		
+		if (Input.GetMouseButtonDown (0) && rayCasted)	// L'utilisateur vient de cliquer
+		{
+			var hitRotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+			Instantiate(paint, hitInfo.point, hitRotation);
+		}
+	}
+}
