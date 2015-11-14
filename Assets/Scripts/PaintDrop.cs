0dd72cedb90993ccb7d52879c99acb7f78cb56ee
Vintache Jean <jean.vintache@etu.utc.fr>
2015-11-14 19:05:16 +0100

    Paint Splashes appear when paintdrops hit objects.\nSome splashes appear under the ground, this is still to be fixed

--- Assets/Scripts/PaintDrop.cs
+++ Assets/Scripts/PaintDrop.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class PaintDrop : MonoBehaviour {
+	public GameObject PaintSplash;
 
 	// Use this for initialization
 	void Start () {
@@ -13,7 +14,21 @@ public class PaintDrop : MonoBehaviour {
 
 	}
 
-	void OnCollisionEnter(){
+	void OnCollisionEnter(Collision colInfo){
+		// Paint only paintable objects
+		if (!colInfo.gameObject.CompareTag("Paintable"))
+			return;
+
+		Debug.Log("Paintdrop collision, "+colInfo.contacts.Length+" points");
+
+		ContactPoint contact = colInfo.contacts[colInfo.contacts.Length - 1];
+
+		Debug.Log("Collision normal : "+contact.normal);
+
+		// Apply a paint splash decal onto the object we collided with
+		Instantiate(PaintSplash, contact.point, Quaternion.FromToRotation(Vector3.up,contact.normal));
+
+		// Destroy this paintdrop
 		Destroy(this.gameObject);
 	}
 }
--- Assets/Scripts/ThrowPaintDrops.cs
+++ Assets/Scripts/ThrowPaintDrops.cs
@@ -5,23 +5,38 @@ public class ThrowPaintDrops : MonoBehaviour {
 	public Transform paintDropPrefab;
 
 	private const int RAYCASTLENGTH = 10;
+	private const int generationRateLimit = 10;
+	private int generationCounter;
 
 	// Use this for initialization
 	void Start () {
-		
+		ResetCounter();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Ray ray = GetComponentInChildren<Camera>().ScreenPointToRay(Input.mousePosition);
-				
 		Debug.DrawRay(ray.origin, ray.direction * RAYCASTLENGTH, Color.green);
 		
 		if (Input.GetMouseButton(0))
 		{
-			Transform drop = Instantiate(paintDropPrefab, ray.origin, Quaternion.identity) as Transform;
-			drop.gameObject.GetComponent<Rigidbody>().AddForce(ray.direction * 300);
+			++generationCounter;
+
+			if (generationCounter > generationRateLimit)
+			{
+				generationCounter = 0;
+				Transform drop = Instantiate(paintDropPrefab, ray.origin + ray.direction * 3, Quaternion.identity) as Transform;
+				drop.gameObject.GetComponent<Rigidbody>().AddForce(ray.direction * 250);
+			}
+		}
+		else if (Input.GetMouseButtonUp(0))
+		{
+			ResetCounter();
 		}
 
+	}
+
+	void ResetCounter() {
+		generationCounter = generationRateLimit;
 	}
 }
