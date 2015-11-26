63eef3831cd97d6f8397b900967ae16874a92c16
atchandj <atchandjou@gmail.com>
2015-11-26 18:33:09 +0100

    modification of the menu

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -1,40 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class MenuScript : MonoBehaviour {
 
+	public Button playText;
+	public Button quitText;
 	// Use this for initialization
 	void Start () {
-		
+		playText = playText.GetComponent<Button> ();
+		quitText = quitText.GetComponent<Button> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		OnGUI ();
+	void Update () {;
 	}
 
-	void OnGUI()
-	{
-		const int buttonWidth = 160;
-		const int buttonHeight = 30;
-		
-		// Affiche un bouton pour démarrer la partie
-		if (
-			GUI.Button(/* Centré en x, 2/3 en y*/ new Rect(Screen.width / 2 - (buttonWidth / 2),
-			(2 * Screen.height / 3) - (buttonHeight / 2),buttonWidth,buttonHeight),"Start!"	)
-			)
-		{
-			// Sur le clic, on démarre le premier niveau
-			// "Stage1" est le nom de la première scène que nous avons créés.
-			Application.LoadLevel("main");
-		}
+	public void StartLevel(){
+		Application.LoadLevel("main");
+	}
 
-		if (
-			GUI.Button(new Rect(Screen.width / 2 - (buttonWidth / 2),
-		    (2 * Screen.height / 3) + (buttonHeight),buttonWidth,buttonHeight),"Quit!"	)
-			)
-		{
-			Application.Quit() ;
-		}
+	public void ExitGame(){
+		Application.Quit() ;
 	}
 }
