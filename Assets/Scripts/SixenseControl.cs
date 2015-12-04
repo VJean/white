2c2b311980427a9e61f0fedda6fabd33d725690f
Vintache Jean <jean.vintache@etu.utc.fr>
2015-12-04 15:52:20 +0100

    Add Razer Hydra input

---
+++ Assets/Scripts/SixenseControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SixenseControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		SixensePlugin.sixenseInit();
+
+		for (int i = 0; i < SixenseInput.Controllers.Length; i++) {
+			Debug.Log (SixenseInput.Controllers[i]);
+		}
+
+		// Debug.Log("Razer Controllers :" + SixenseInput.Controllers.Length);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
